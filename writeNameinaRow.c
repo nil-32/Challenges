9742d6f94fc46a088af1df6a1a444aaae2469c28
1Bartu_Paçal3 <118813470+BartuPacall@users.noreply.github.com>
2022-12-13 10:21:37 +0300

    Update writeNameinaRow.c

--- writeNameinaRow.c
+++ writeNameinaRow.c
@@ -1,17 +1,45 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 int main(){
- int i,j,k,n; 
- printf("Enter the number of the row :");
- scanf("%d",&n);
- 
- for(i=0;i<n;i++){
-     for(j=0;i<n;i++){
-     
-     
- }
-     printf("Akif")
- }
- 
-    
+int i,j,k,n;
+printf("Enter the number of the row :");
+scanf("%d",&n);
+
+for(i=0;i<n;i++){
+for(j=0;j<n;j++){
+
+}
+printf("Akif\n");
+}
+
+}
+----------------------------------------------------------------
+ #include <stdio.h>
+int main(){
+int i,j,k,n;
+printf("Enter the number of the row :");
+scanf("%d",&n);
+
+for(i=0;i<n;i++){
+for(j=0;j<n;j++){
+
+}
+printf("Akif\t");
+}
+
+}
+-----------------------------------------------------------------
+ #include <stdio.h>
+int main(){
+int i,j,k,n;
+printf("Enter the number of the row :");
+scanf("%d",&n);
+
+for(i=0;i<n;i++){
+for(j=0;j<n;j++){
+
+}
+printf("Akif");
+}
+
 }
