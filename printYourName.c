8e6a6f5fc104bfb238f4a0352bfbceb6ebe20b60
1Bartu_Paçal3 <118813470+BartuPacall@users.noreply.github.com>
2022-12-13 10:02:30 +0300

    Update printYourName.c

--- printYourName.c
+++ printYourName.c
@@ -1,1 +1,12 @@
 // your name surname and age from user
+#include "stdio.h"
+
+int main()
+{
+    char name[10];
+    char surname[10];
+    printf("Enter your name and surname:");
+    scanf("%s %s",&name,&surname);
+    printf("My name:%s My surname:%s",name,surname);
+    return 0;
+}
