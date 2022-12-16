bf792050585f68562ab267ca2ff052535e1c675d
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-16 20:36:19 +0300

    Create pointer.c

---
+++ pointer.c
@@ -0,0 +1,13 @@
+// Please read this file as well
+
+
+#include<stdio.h>
+
+int main() {
+    char *ptr[] = {"C", "language", "is", "fun", "than","C#"};
+  
+    for (int i = 0; i <= 5; i++) {
+        printf("%s ", ptr[i]);
+    }
+    return 0;
+}
